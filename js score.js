43f50812bca2f1fd3a0983e0e25c25d3d9d1ea45
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-08-08 17:38:38 +0530

    Add files via upload

---
+++ js score.js
@@ -0,0 +1,33 @@
+
+function total()
+{
+    var a=document.getElementById("phy").value
+    var b=document.getElementById("chem").value
+    var c=document.getElementById("math").value
+    var d=document.getElementById("bio").value
+    a=parseInt(a)
+    b=parseInt(b)
+    c=parseInt(c)
+    d=parseInt(d)
+    var total=a+b+c+d
+    var average=total/4
+   
+    document.getElementById("total").innerHTML='Total is='+total
+   
+}
+function average()
+{
+    var a=document.getElementById("phy").value
+    var b=document.getElementById("chem").value
+    var c=document.getElementById("math").value
+    var d=document.getElementById("bio").value
+    a=parseInt(a)
+    b=parseInt(b)
+    c=parseInt(c)
+    d=parseInt(d)
+    var total=a+b+c+d
+    var average=total/4
+   
+    
+    document.getElementById("average").innerHTML='Average is='+average
+}
