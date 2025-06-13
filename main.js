ba979fc21176371e96b14d26d1d8314d4ab91472
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-06-13 19:21:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let val;
+// val = document;
+// val = document.all;
+// val = document.all[0];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].className;
+// val = document.links[0].classList;
+// val = document.scripts;
+// val = document.scripts[2].getAttribute("src");
+// console.log(val);
+
+//Single element selector
+//document.getElementById
+//document.querySelector
+
+//Selecting multiple elements
+//documnet.getElementsByClassNames('___');
+//document.getElmentsByTagNames("__");
+
+//document.querySelecotorAll("li.collection-item");  //No need to convert to an array
+//console.log(lis);
+
+
+//DOM Traversing
+
+const list
